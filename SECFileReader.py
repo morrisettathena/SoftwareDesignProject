04616964f83add9ff29f1e86066998891953016b
dle245 <120441525+dle245@users.noreply.github.com>
2023-04-06 16:02:10 -0400

    Update SECFileReader.py
    Is using Pandas Dataframe instead of Numpy and doesn't alter the data file. Has an issue where it reads almost all of the data except the last column.

--- SECFileReader.py
+++ SECFileReader.py
@@ -1,9 +1,26 @@
-import numpy as np
+import pandas as pd
 
-f = open("COMSC110S20.SEC","r")
-bruh = f.read()
-bruh = bruh.replace(","," ")
-bruh = bruh.replace("\"", "")
-with open(r'COMSC110S20.SEC', 'w') as file:
-    file.write(bruh)
-bruh2 = np.loadtxt("COMSC110S20.SEC", skiprows=1,dtype='str')
+def getData(data): #converts data into a list
+    list1 = []
+    
+    for x in range(6):
+        list2 = data.iloc[x][0].split(",")
+        list1 = list1 + list2
+    return list1
+
+def addData(data): #Converts the list into a pandas Dataframe
+    data.pop(0)
+    
+    bruh3 = {"First Name":[], "Last Name":[], "ID":[], "Grades":[]}
+    
+    df = pd.DataFrame(bruh3)
+    
+    for x in range(0, len(data), 4):
+        df.loc[x] = [data[x], data[x+1], data[x+2], data[x+3]]
+    return df
+
+bruh = pd.read_csv("COMSC110S21.SEC", sep=" ", header=None)
+
+bruh2 = getData(bruh) 
+
+df = addData(bruh2)
