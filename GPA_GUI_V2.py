24d3fb8af733105b5725d54301eb23115d251fe1
tmacgregor413 <90424646+tmacgregor413@users.noreply.github.com>
2023-05-01 15:55:27 -0400

    Update GPA_GUI_V2.py

--- GPA_GUI_V2.py
+++ GPA_GUI_V2.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[4]:
-
-
 # USE
 
 #__________________________________________________________TITLES_________________________________________________________#
@@ -17,13 +11,13 @@ root.title("GPA Calculator")
 root.config(bg='#CACBD1')
 root.attributes('-fullscreen', True)
 
-# Create title label
+# ROGER WILLIAMS LABEL
 title_label = ttk.Label(root, text="   Roger Williams", font=("Times New Roman", 20), foreground="white", background="#1E3261")
-title_label.grid(row=1, column=0, columnspan=5, pady=(30, 0), sticky="nsew")
+title_label.grid(row=1, column=0, columnspan=5, pady=(10, 0), sticky="nsew")
 
-# Create university label
+# UNIVERSITY LABEL
 univ_label = ttk.Label(root, text="  University", font=("Times New Roman", 27), foreground="#60ADF0", background="#1E3261")
-univ_label.grid(row=2, column=0, columnspan=5, pady=(0, 40), sticky="nsew")
+univ_label.grid(row=2, column=0, columnspan=5, pady=(0, 45), sticky="nsew")
 
 
 #__________________________________________________________TEXTBOXES______________________________________________________#
@@ -42,7 +36,7 @@ dir_box.grid(row=3, column=0, padx=20, pady=10, sticky="w")
 file_box = tk.Text(root, height=12, font=("Arial", 14), bg="white", fg="#a3a3a3", bd=0)
 file_box.insert(tk.END, " File contents will be displayed here.")
 file_box.bind("<FocusIn>", lambda event: file_box.delete(1.0, tk.END))
-file_box.grid(row=5, column=0, padx=20, pady=20, sticky="w", columnspan=2)
+file_box.grid(row=5, column=0, padx=20, pady=30, sticky="w", columnspan=2)
 
 # CALCULATION BOX
 calc_box = tk.Text(root, height=12, font=("Arial", 14), bg="white", fg="#a3a3a3", bd=0)
@@ -55,7 +49,7 @@ bot_space = bot_space.grid(row=7, column=0, padx=5, pady=5, sticky="ew")
 
 # GRAPH BOX
 graph_box = tk.Entry(root, width=100, bg="white", fg="white", bd=0)
-graph_box = graph_box.grid(row=3, rowspan=4, column=3, padx=5, pady=10, sticky="nse")
+graph_box = graph_box.grid(row=3, rowspan=4, column=3, columnspan=2, padx=20, pady=10, sticky="nse")
 
 #________________________________________________________FUNCTIONS________________________________________________________#
 
@@ -99,10 +93,9 @@ clear_button.grid(row=4, column=0, pady=10, padx=20, sticky="e")
 
 # EXIT BUTTON
 exit_button = ttk.Button(root, text="Exit", command=exit_program, width=10)
-exit_button.grid(row=1, column=4, sticky="nw", padx=7, pady=7)
+exit_button.grid(row=1, column=4, sticky="ne", padx=7, pady=7)
 
 #___________________________________________________________ATTACH________________________________________________________#
-    
     
 # Attach select_file function to browse button
 browse_button.config(command=select_file)
@@ -124,4 +117,3 @@ root.rowconfigure(3, weight=1)
 
 
 root.mainloop()
-
