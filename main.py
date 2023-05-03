9316e3d6238059f7b890294f04cba241d7c9b2ff
morrisettjohn <60852062+morrisettjohn@users.noreply.github.com>
2023-05-03 19:32:42 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,38 +2,12 @@ import reader
 import globals as g
 import calculation
 import util as u
-from collections import OrderedDict
 
 PRINTSECTIONSTATS = True
 PRINTGROUPSTATS = True
 DEBUG = True
 
-def getFolder():
-    if DEBUG:
-        return "./data"
-    else:
-        return input("Select folder")
-    
-def selectRun(runData: list):
-    while True:
-        print("Select run file: ")
-        
-        print(", ".join(runData))
-        runFile = input("Choice (name or number):")
-        if runFile.isdigit():
-            index = int(runFile)
-            if index < len(runData) and index >= 0:
-                return runData[index]
-            else:
-                print("index out of bounds")
-        else:
 
-            if runFile not in runData:
-                print("not a valid run file")
-            else:
-                return runFile
-        print()
-        
 def checkGrps(folderData: dict, grps: list):
 
     for grp in grps:
@@ -46,30 +20,6 @@ def checkSecs(folderData: dict, secs: list):
         if sec not in folderData[g.SEC_EXT]:
             return False
     return True
-
-def useSameFolder():
-    return input("Use same folder? y/n").lower().startswith("y")
-
-def printSecStats(secData: dict):
-    for item in secData:
-        print("\n" + item + " stats:")
-        for i in secData[item]:
-            if type(secData[item][i]) == dict:
-                print(i + ": " + str(OrderedDict(sorted(secData[item][i].items()))))
-            else:
-                print(i + ": " + str(secData[item][i]))
-
-        print("*"*30)
-
-def printGrpStats(grpData: dict):
-    for item in grpData:
-        print("\n" + item + " stats:")
-        for i in grpData[item]:
-            if type(grpData[item][i]) == dict:
-                print(i + ": " + str(OrderedDict(sorted(grpData[item][i].items()))))
-            else:
-                print(i + ": " + str(grpData[item][i]))
-        print("*"*30)
 
 def fetch(originalpath: str):
         runFile = originalpath[originalpath.rindex("/")+1:]
